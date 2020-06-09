732ea3403acb3c0ed074c32bd04cd3b9ee599f00
ana-co <adavi18@freeuni.edu.ge>
2020-06-09 16:10:21 +0400

    chat table and model updated

--- src/main/java/ge/exen/models/Chat.java
+++ src/main/java/ge/exen/models/Chat.java
@@ -3,35 +3,35 @@ package ge.exen.models;
 
 public class Chat {
 
-  private long chatId;
-  private long from;
-  private long to;
+    private long chatId;
+    private long user1;
+    private long user2;
 
 
-  public long getChatId() {
-    return chatId;
-  }
+    public long getChatId() {
+        return chatId;
+    }
 
-  public void setChatId(long chatId) {
-    this.chatId = chatId;
-  }
-
-
-  public long getFrom() {
-    return from;
-  }
-
-  public void setFrom(long from) {
-    this.from = from;
-  }
+    public void setChatId(long chatId) {
+        this.chatId = chatId;
+    }
 
 
-  public long getTo() {
-    return to;
-  }
+    public long getUser1() {
+        return user1;
+    }
 
-  public void setTo(long to) {
-    this.to = to;
-  }
+    public void setUser1(long user1) {
+        this.user1 = user1;
+    }
+
+
+    public long getUser2() {
+        return user2;
+    }
+
+    public void setUser2(long user2) {
+        this.user2 = user2;
+    }
 
 }
