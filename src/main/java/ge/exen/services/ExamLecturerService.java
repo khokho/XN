835405bcfafc0ac8d914ebffbd02f45fdcd05537
tamtatop <ttopu18@freeuni.edu.ge>
2020-07-17 14:01:08 +0400

    added ExamLecturerService and started tests for postService an ExamLecturerService

---
+++ src/main/java/ge/exen/services/ExamLecturerService.java
@@ -0,0 +1,46 @@
+package ge.exen.services;
+
+import ge.exen.DAO.ExamDao;
+import ge.exen.DAO.ExamLecturersDAO;
+import ge.exen.DAO.UserDAO;
+import ge.exen.dto.ExamLecturersRegisterDTO;
+import ge.exen.models.Exam;
+import ge.exen.models.ExamLecturers;
+import ge.exen.models.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ExamLecturerService implements IExamLecturerService{
+    @Autowired
+    ExamDao examDao;
+
+    @Autowired
+    IUserService userService;
+
+    @Autowired
+    UserDAO userDAO;
+
+    @Autowired
+    ExamLecturersDAO examLecturersDAO;
+
+    @Override
+    public boolean assignLecturerToExam(ExamLecturersRegisterDTO registerDTO){
+        //check if exam exists
+        Exam exam =  examDao.get(registerDTO.getExamId());
+        if(exam == null) return false;
+
+        Long examId = exam.getID();
+        //check if user with this mail exists
+        if(!userService.checkMailExists(registerDTO.getLecturerMail())) return false;
+
+        Long lectId = userDAO.getUserByMail(registerDTO.getLecturerMail()).getId();
+        //check if user with this mail is a lecturer
+        if(!userDAO.getStatusByUserId(lectId).equals(User.LECTURER)) return false;
+
+        ExamLecturers examLecturer = new ExamLecturers(examId, lectId);
+        if(!examLecturersDAO.create(examLecturer)) return false;
+
+        return true;
+    }
+}
