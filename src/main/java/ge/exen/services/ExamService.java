1faf14a497e9fcbbe7e4d1a0dbcabf3e814e631d
sHOTa-23 <55255354+sHOTa-23@users.noreply.github.com>
2020-07-19 20:52:20 +0400

    finnished examservice's easy functions

--- src/main/java/ge/exen/services/ExamService.java
+++ src/main/java/ge/exen/services/ExamService.java
@@ -12,7 +12,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 
-
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
@@ -72,9 +71,7 @@ public class ExamService implements IExamService {
         List<Exam> exams = dao.getAll();
         for (int i = 0; i < exams.size(); i++) {
             StudentExam exam = studentExamDao.get(user.getId(), exams.get(i).getID());
-            if (exam != null) {
-                return exam;
-            }
+            if (exam != null) return exam;
         }
         return null;
     }
@@ -85,11 +82,27 @@ public class ExamService implements IExamService {
         List<Exam> ans = new ArrayList<>();
         for (int i = 0; i < exams.size(); i++) {
             String startDate = exams.get(i).getStartDate();
+            int duration = exams.get(i).getDurationInMinutes();
             DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm");
             LocalDateTime now = LocalDateTime.now();
-         //   Date date = dtf.format(now);
-           // System.out.println(dtf.format(now));
+            String currentTime = dtf.format(now);
+            int indexOfSpace1 = startDate.indexOf(" ");
+            int indexOfSpace2 = currentTime.indexOf(" ");
+            String date1 = startDate.substring(0, indexOfSpace1);
+            String date2 = currentTime.substring(0, indexOfSpace2);
+          //  System.out.println("axla droa: " + currentTime + "  axla: " + startDate + " shemcirebulebi " + date2 + " " + date1);
+            if (date1.equals(date2)) {
+                String inHours = startDate.substring(indexOfSpace1 + 1);
+                String inHours1 = currentTime.substring(indexOfSpace2 + 1);
+               // System.out.println(inHours + " " + inHours1);
+                int inMinutes = 60 * Integer.parseInt(inHours.substring(0, inHours.indexOf(":"))) +
+                        Integer.parseInt(inHours.substring(inHours.indexOf(":") + 1));
+                int inMinutesNow = 60 * Integer.parseInt(inHours1.substring(0, inHours1.indexOf(":"))) +
+                        Integer.parseInt(inHours1.substring(inHours1.indexOf(":") + 1));
+               // System.out.println("match moxda : " + date1 + " " + date2 + " " + inMinutes + " " + inMinutesNow);
+                if (inMinutes + duration <= inMinutesNow) ans.add(exams.get(i));
+            }
         }
-        return null;
+        return ans;
     }
 }
