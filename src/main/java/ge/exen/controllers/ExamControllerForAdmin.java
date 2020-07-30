a4184979ca3eb3e2e2a4d84ac8777c036f6407d2
sHOTa-23 <55255354+sHOTa-23@users.noreply.github.com>
2020-07-31 03:09:12 +0400

    added some parsing

--- src/main/java/ge/exen/controllers/ExamControllerForAdmin.java
+++ src/main/java/ge/exen/controllers/ExamControllerForAdmin.java
@@ -18,29 +18,59 @@ public class ExamControllerForAdmin {
     private int EXAMS_PER_PAGE = 10;
     @Autowired
     IExamService examService;
+
     @GetMapping(value = "/l")
     public String toAdminHomePage(HttpServletRequest req) {
         List<Exam> exams = examService.getAllExams();
-        int pageNum = exams.size()/EXAMS_PER_PAGE+1;
-        req.setAttribute("pageNum",pageNum);
-        req.setAttribute("content","ExamsView.jsp");
-        req.setAttribute("sidebar","adminSidebar.jsp");
+        List<Pair> list = new ArrayList<>();
+        for(int i = 0; i < exams.size(); i++){
+            list.add(new Pair(exams.get(i),examService.isCurrentlyLive(exams.get(i))));
+        }
+        int pageNum = exams.size() / EXAMS_PER_PAGE + 1;
+        req.setAttribute("list", list);
+        req.setAttribute("pageNum", pageNum);
+        req.setAttribute("content", "ExamsView.jsp");
+        req.setAttribute("sidebar", "adminSidebar.jsp");
         return "template";
     }
+
     @PostMapping(value = "/r")
     public String rendeerExams(HttpServletRequest req) {
         int startIndex = 0;
-        if(req.getAttribute("page") !=null)  startIndex = EXAMS_PER_PAGE*((int)req.getAttribute("page")-1);
-        List<Exam> list =  new ArrayList<>();
-        for(int i = startIndex; i < startIndex+EXAMS_PER_PAGE; i++) {
+        if (req.getAttribute("page") != null) startIndex = EXAMS_PER_PAGE * ((int) req.getAttribute("page") - 1);
+        List<Exam> list = new ArrayList<>();
+        for (int i = startIndex; i < startIndex + EXAMS_PER_PAGE; i++) {
             list.add(examService.getAllCurrentExams().get(i));
         }
-       // req.setAttribute("list",new ArrayList<Exam>().addAll(startIndex,));
+        // req.setAttribute("list",new ArrayList<Exam>().addAll(startIndex,));
         return "";
     }
 
 
+    public class Pair {
+        private Exam exam;
+        private boolean isCurrentlyOn;
 
+        public Pair(Exam exam, boolean isCurrentlyOn) {
+            this.exam = exam;
+            this.isCurrentlyOn = isCurrentlyOn;
+        }
 
+        public Exam getExam() {
+            return exam;
+        }
+
+        public void setExam(Exam exam) {
+            this.exam = exam;
+        }
+
+        public boolean isCurrentlyOn() {
+            return isCurrentlyOn;
+        }
+
+        public void setCurrentlyOn(boolean currentlyOn) {
+            isCurrentlyOn = currentlyOn;
+        }
+    }
 
 }
