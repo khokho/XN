58d69878278b1df1485c632d1c25e82cb619b38b
Aleksandre Khokhiashvili <alkhok18@freeuni.edu.ge>
2020-08-01 00:31:37 +0400

    partial buttons

--- src/main/webapp/resources/js/queues_student_buttons.js
+++ src/main/webapp/resources/js/queues_student_buttons.js
@@ -2,13 +2,17 @@
 
 const e = React.createElement;
 
-//var ws = new SockJS("/ws");
-//var stomp = Stomp.over(ws);
-
-
+var ws = new SockJS("/ws");
+var stomp = Stomp.over(ws);
 
 function enableButton(obj, disabled) {
     obj.setState({disabled})
+}
+
+//this function will be bound to button !this!
+function onmessage(responseJSON){
+    console.log("Hi my name is bob and I'm from wesocket")
+    this.updateBtn()
 }
 
 class EnqueueButton extends React.Component {
@@ -17,6 +21,11 @@ class EnqueueButton extends React.Component {
         this.state = {disabled: false};
         this.queueName = props.queueName;
         this.handleClick = this.handleClick.bind(this);
+    }
+
+    startListen(){
+        const my_onmessage = onmessage.bind(this);
+        stomp.subscribe("/topic/queue-" + this.queueName, my_onmessage, {});
     }
 
     handleClick() {
@@ -48,9 +57,19 @@ class EnqueueButton extends React.Component {
 class CancelButton extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {disabled: true};
+        this.state = {disabled: true, count: 0};
         this.queueName = props.queueName;
         this.handleClick = this.handleClick.bind(this);
+
+
+
+        console.log('obj:' + props.queueName)
+
+    }
+
+    startListen(){
+        const my_onmessage = onmessage.bind(this);
+        stomp.subscribe("/topic/queue-" + this.queueName, my_onmessage, {});
     }
 
     handleClick() {
@@ -72,33 +91,57 @@ class CancelButton extends React.Component {
         }
     }
 
+    update(){
+        var button = this
+        fetch('http://' + window.location.host + '/getqueuesize')
+            .then(resp=>{
+                return resp.json()
+            })
+            .then(res=>{
+                console.log(res)
+                button.setState({count:res})
+            })
+
+
+        fetch('http://' + window.location.host + '/getdisabled')
+            .then(resp=>{
+                return resp.json()
+            })
+            .then(res=>{
+                console.log(res)
+                button.setState({disabled:res})
+            })
+
+    }
+
     render() {
         return (
-            <button disabled={this.state.disabled} onClick={this.handleClick}> "მოთხოვნის გაუქმება" </button>
+            <div>
+                <button disabled={this.state.disabled} onClick={this.handleClick}> "მოთხოვნის გაუქმება" </button>
+                <p>count: {this.state.count}</p>
+            </div>
         );
     }
 }
 
-
-
 var paperEnq = ReactDOM.render(
-    <EnqueueButton queueName='blank paper' />,
+    <EnqueueButton queueName='blank-paper' />,
     document.getElementById('paper-enqueue')
 );
 
 var paperCancel = ReactDOM.render(
-    <CancelButton queueName='blank paper' />,
+    <CancelButton queueName='blank-paper' />,
     document.getElementById('paper-cancel')
 );
 
 var examerEnq = ReactDOM.render(
-    <EnqueueButton queueName='call examer' />,
+    <EnqueueButton queueName='call-examer' />,
     document.getElementById('examer-enqueue')
 );
 
 
 var examerCancel = ReactDOM.render(
-    <CancelButton queueName='call examer'/>,
+    <CancelButton queueName='call-examer'/>,
     document.getElementById('examer-cancel')
 );
 
@@ -111,3 +154,15 @@ var wcCancel = ReactDOM.render(
     <CancelButton queueName='wc' />,
     document.getElementById('wc-cancel')
 );
+
+//this gets called when socket connects
+stomp.connect({}, function () {
+    paperEnq.startListen()
+    examerEnq.startListen()
+    wcEnq.startListen()
+
+    paperCancel.startListen()
+    examerCancel.startListen()
+    wcCancel.startListen()
+    wcCancel.update()
+});
